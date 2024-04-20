b0ad1472f1f8a98357a478158cf16a0159c0ee2c
HiCurl <i.got.a.little.carried.away@gmail.com>
2024-04-20 03:21:23 +0000

    Timerクラス定義

---
+++ src/models/Timer.ts
@@ -0,0 +1,27 @@
+export class Timer {
+  private readonly thick: number;
+  private interval: NodeJS.Timeout | undefined = undefined;
+
+  constructor(thick: number = 1000) {
+    this.thick = thick;
+  }
+
+  start(time: number, onThick: (current: number) => void, onEnd: () => void = () => {}) {
+    this.interval = setInterval(() => {
+      time -= this.thick;
+      onThick(time);
+
+      if (time <= 0) {
+        clearInterval(this.interval);
+        onEnd();
+        return;
+      }
+    }, this.thick);
+  }
+
+  stop() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+}
