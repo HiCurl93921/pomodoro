7f85f11ec088b0a29e22a9846e90c6e8f103fc97
HiCurl <i.got.a.little.carried.away@gmail.com>
2024-04-21 06:09:09 +0000

    セミコロン削除

--- src/models/Timer.ts
+++ src/models/Timer.ts
@@ -1,27 +1,31 @@
 export class Timer {
-  private readonly thick: number;
-  private interval: NodeJS.Timeout | undefined = undefined;
+	private readonly thick: number
+	private interval: NodeJS.Timeout | undefined = undefined
 
-  constructor(thick: number = 1000) {
-    this.thick = thick;
-  }
+	constructor(thick: number = 1000) {
+		this.thick = thick
+	}
 
-  start(time: number, onThick: (current: number) => void, onEnd: () => void = () => {}) {
-    this.interval = setInterval(() => {
-      time -= this.thick;
-      onThick(time);
+	start(
+		time: number,
+		onThick: (current: number) => void,
+		onEnd: () => void = () => {},
+	) {
+		this.interval = setInterval(() => {
+			time -= this.thick
+			onThick(time)
 
-      if (time <= 0) {
-        clearInterval(this.interval);
-        onEnd();
-        return;
-      }
-    }, this.thick);
-  }
+			if (time <= 0) {
+				clearInterval(this.interval)
+				onEnd()
+				return
+			}
+		}, this.thick)
+	}
 
-  stop() {
-    if (this.interval) {
-      clearInterval(this.interval);
-    }
-  }
+	stop() {
+		if (this.interval) {
+			clearInterval(this.interval)
+		}
+	}
 }
