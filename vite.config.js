f9920b741f247b06315acc605ad23d47f4a71516
HiCurl <i.got.a.little.carried.away@gmail.com>
2024-05-06 06:32:58 +0000

    設定ファイル更新

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		watch: {
-			usePolling: true
-		}
+			usePolling: true,
+		},
 	},
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-});
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+	},
+})
